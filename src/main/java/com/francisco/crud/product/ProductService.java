e22bc3d855b6bd5034565c28dd79989636fe4c1d
Camila Saenz <farguedas99@gmail.com>
2023-08-28 17:49:45 -0600

    Service added

---
+++ src/main/java/com/francisco/crud/product/ProductService.java
@@ -0,0 +1,16 @@
+package com.francisco.crud.product;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @GetMapping
+    public List<Product> getProducts(){
+        return List.of(new Product(2541L,"Desktop PC",500, LocalDate.of(2025, Month.MARCH, 5),2));
+    }
+}
