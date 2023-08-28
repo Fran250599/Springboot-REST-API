6fee8812816dd36ddd26b0dae518cffaaf570da0
Camila Saenz <farguedas99@gmail.com>
2023-08-28 17:15:01 -0600

    Spring web project initialized

---
+++ src/main/java/com/francisco/crud/product/Product.java
@@ -0,0 +1,69 @@
+package com.francisco.crud.product;
+
+import java.time.LocalDate;
+
+public class Product {
+    private long id;
+    private String name;
+    private String price;
+    private LocalDate date;
+    private int age;
+
+    public Product() {
+    }
+
+    public Product(long id, String name, String price, LocalDate date, int age) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.date = date;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Product(String name, String price, LocalDate date, int age) {
+        this.name = name;
+        this.price = price;
+        this.date = date;
+        this.age = age;
+    }
+}
