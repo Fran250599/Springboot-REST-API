ac03cc75850e72ad1da187ae67939398ba790db4
Francisco Arguedas <farguedas99@gmail.com>
2023-11-11 13:07:55 -0600

    User endpoints working as expected

---
+++ src/main/java/com/francisco/crud/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.francisco.crud.controller;
+
+import com.francisco.crud.model.User;
+import com.francisco.crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Object> getUserById( @PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> registerUser(@RequestBody User user){
+        return userService.newUser(user);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id, @RequestBody User user){
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id){
+        return userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/francisco/crud/service/UserService.java
@@ -0,0 +1,119 @@
+package com.francisco.crud.service;
+
+import com.francisco.crud.model.User;
+import com.francisco.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public ResponseEntity<Object> getUserById(Long id){
+        Optional<User> res = userRepository.findById(id);
+        HashMap<String, Object> data = new HashMap<>();
+
+        if(res.isPresent()){
+            data.put("Dato encontrado:", res.get());
+            data.put("Message", "Usuario encontrado.");
+            return new ResponseEntity<>(
+                    data,
+                    HttpStatus.OK
+            );
+        }
+        data.put("Error", true);
+        data.put("Message", "No existe un usuario con este id.");
+        return new ResponseEntity<>(
+                data,
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    public ResponseEntity<Object> newUser(User user){
+        Optional<User> res = userRepository.findUserByUsername(user.getUsername());
+        HashMap<String, Object> data = new HashMap<>();
+
+        if(res.isPresent()){
+            data.put("Error", true);
+            data.put("Message", "Ya existe un usuario con este nombre de usuario.");
+            return new ResponseEntity<>(
+                    data,
+                    HttpStatus.CONFLICT
+            );
+        }
+        userRepository.save(user);
+        data.put("Message", "Usuario creado.");
+        data.put("Dato creado:", user);
+        return new ResponseEntity<>(
+                data,
+                HttpStatus.CREATED
+        );
+    }
+
+    // Update user
+    public ResponseEntity<Object> updateUser(Long id, User user){
+        Optional<User> res = userRepository.findById(id);
+        HashMap<String, Object> data = new HashMap<>();
+
+        if(res.isPresent()){
+            User userToUpdate = res.get();
+            userToUpdate.setUsername(user.getUsername());
+            userToUpdate.setPassword(user.getPassword());
+            userToUpdate.setRole(user.getRole());
+            userRepository.save(userToUpdate);
+            data.put("Message", "Usuario actualizado.");
+            data.put("Dato actualizado:", userToUpdate);
+            return new ResponseEntity<>(
+                    data,
+                    HttpStatus.OK
+            );
+        }
+        data.put("Error", true);
+        data.put("Message", "No existe un usuario con este id.");
+        return new ResponseEntity<>(
+                data,
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    // Delete user
+    public ResponseEntity<Object> deleteUser(Long id){
+        Optional<User> res = userRepository.findById(id);
+        HashMap<String, Object> data = new HashMap<>();
+
+        if(res.isPresent()){
+            userRepository.delete(res.get());
+            data.put("Message", "Usuario eliminado.");
+            data.put("Dato eliminado:", res.get());
+            return new ResponseEntity<>(
+                    data,
+                    HttpStatus.OK
+            );
+        }
+        data.put("Error", true);
+        data.put("Message", "No existe un usuario con este id.");
+        return new ResponseEntity<>(
+                data,
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+}
